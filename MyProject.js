6e69f6935ab183d467554eacee54fd9e322c9a0b
insertcoinhere <137066605+insertcoinhere@users.noreply.github.com>
2023-10-27 11:51:30 +0800

    My Project

---
+++ MyProject.js
@@ -0,0 +1,21 @@
+function bubbleSort(arr){
+
+    //Outer pass
+    for(let i = 0; i < arr.length; i++){
+
+        //Inner pass
+        for(let j = 0; j < arr.length - i - 1; j++){
+
+            //Value comparison using ascending order
+
+            if(arr[j + 1] < arr[j]){
+
+                //Swapping
+                [arr[j + 1],arr[j]] = [arr[j],arr[j + 1]]
+            }
+        }
+    };
+    return arr;
+};
+
+const string2 = bubbleSort([30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]);
---
+++ README.txt
@@ -0,0 +1,5 @@
+DATE: October 27, 2023<br>
+Proposed Thesis Title: 'Musta: Learn the Filipino Language through RPG
+
+Sorting Algorithm: Bubble Sort
+It works by repeatedly iterating through the array and comparing pairs of elements, swapping their positions if they are out of order. This process is repeated until the entire array is sorted.
